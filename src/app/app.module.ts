a6a57fc32a30cbc66715e325f414800193e858a3
luisriego <luisriego@hotmail.com>
2019-04-05 11:48:55 -0300

    Creados componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadComponent } from './shared/bread/bread.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotfoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadComponent
   ],
   imports: [
     BrowserModule,
