a704bb9b4d2f1a0bf2fa24985bed06c4aaae50c9
luisriego <luisriego@hotmail.com>
2019-04-05 14:47:19 -0300

    Inclusión de la plantilla Adminpro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Services
+import { SharedService } from './services/shared.service';
+import { SidebarService } from './services/sidebar.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NotfoundComponent } from './notfound/notfound.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -26,7 +30,10 @@ import { BreadComponent } from './shared/bread/bread.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    SharedService,
+    SidebarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
